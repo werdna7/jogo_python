0ce40b295f893d05c3951a5652322ef7c12ae7b9
werdna7 <andrew.aapolo@gmail.com>
2024-10-17 22:25:04 -0300

    Tabuleiro

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -6,14 +6,14 @@ import pygame
 pygame.init() #inicialização do pygame
 pygame.font.init() #inicialização do pacote de fontes no pygame
 
-screen = pygame.display.set_mode((500, 500)) #definição do tamanho da tela
+screen = pygame.display.set_mode((600, 600)) #definição do tamanho da tela
 pygame.display.set_caption('Jogo da Velha') #nome da janela
 clock = pygame.time.Clock() #biblioteca de tempo
 
-fonte_quadrinhos = pygame.font.SysFont('Comic Sans Ms', 30) #importar 
+fonte_quadrinhos = pygame.font.SysFont('Comic Sans Ms', 100, True, True) #importar 
 running = True #variavel de controle do status do jogo
 
-personagem_x = fonte_quadrinhos.render('Δ', True, 'red')
+personagem_x = fonte_quadrinhos.render('x', True, 'red')
 personagem_y = fonte_quadrinhos.render('o', True, 'red')
 cor_fundo = 1
 
@@ -28,18 +28,21 @@ while running:
             cor_fundo = cor_fundo + 1
             if(cor_fundo > 3):
                 cor_fundo = 1
- 
-    if cor_fundo == 1:
-       # screen.fill('blue')
-       screen.blit(personagem_x,(240,240))
-    elif cor_fundo == 2:
-       # screen.fill('red')
-       screen.blit(personagem_y,(240,240))
-    else:
-       screen.fill('purple')
+    
+    pygame.draw.line(screen, 'white',(200, 0), (200, 600), 10)
+    pygame.draw.line(screen, 'white',(400, 0), (400, 600), 10)
+    pygame.draw.line(screen, 'white',(0, 200), (600, 200), 10)
+    pygame.draw.line(screen, 'white',(0, 400), (600, 400), 10)
+       
+
+    screen.blit(personagem_x,(65, 20))
+    screen.blit(personagem_y,(265, 20))
+
+       
+    #else:
+       #screen.fill('black')
 
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
